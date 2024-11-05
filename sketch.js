d2ae3d8bcc4733191620daa0f5c93dfc4161002c
Matthew Lee <Matthew_Lee@avenues-ny.org>
2024-11-05 09:11:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,11 +6,19 @@ let circle1_x, circle1_y, circle1_dx, circle1_dy, circle1_color, circle1_s, circ
 // Circle 2 (CannonBall)
 let circle2_x, circle2_y, circle2_dx, circle2_dy, circle2_color, circle2_s, circle2_r
 // Other Variables
-let w, distance
+let w, distance, c, n, low, high
 
 function setup() {  
   w = 400
   createCanvas(w, w)
+  // Max Color
+  c = 255
+  // Flip to Negative
+  n = -1
+
+  // Velocities
+  low = -3
+  high = 3
   
   // Circle 1 (Cannon) Variables
   circle1_s = 60
@@ -43,20 +51,20 @@ function draw() {
   
   // Boundary Check: Circle 1
   if (circle1_x - circle1_r < 0 || circle1_x + circle1_r > width) { 
-    circle1_dx *= -1
+    circle1_dx *= n
   }
   
   if (circle1_y - circle1_r < 0 || circle1_y + circle1_r > height) { 
-    circle1_dy *= -1
+    circle1_dy *= n
   }
   
   // Boundary Check: Circle 2
   if (circle2_x - circle2_r < 0 || circle2_x + circle2_r > width) { 
-    circle2_dx *= -1
+    circle2_dx *= n
   }
   
   if (circle2_y - circle2_r < 0 || circle2_y + circle2_r > height) { 
-    circle2_dy *= -1
+    circle2_dy *= n
   }
   
   // Collision Check
@@ -69,15 +77,15 @@ function draw() {
 // Collision handling - changes color and makes circles go in opposite directions
 function handleCollision() {
   // Change both circles' colors to random colors
-  circle1_color = color(random(255), random(255), random(255))
-  circle2_color = color(random(255), random(255), random(255))
+  circle1_color = color(random(c), random(c), random(c))
+  circle2_color = color(random(c), random(c), random(c))
   
   // Make circles go in opposite directions by swapping and reversing velocities
-  circle1_dx *= -1
-  circle1_dy *= -1
+  circle1_dx *= n
+  circle1_dy *= n
   
-  circle2_dx *= -1
-  circle2_dy *= -1
+  circle2_dx *= n
+  circle2_dy *= n
 
 }
 
@@ -86,13 +94,13 @@ function reset() {
   // Set random initial positions and velocities
   circle1_x = random(w)
   circle1_y = random(w)
-  circle1_dx = random(-3, 3)
-  circle1_dy = random(-3, 3)
+  circle1_dx = random(low, high)
+  circle1_dy = random(low, high)
   
   circle2_x = random(w)
   circle2_y = random(w)
-  circle2_dx = random(-3, 3)
-  circle2_dy = random(-3, 3)
+  circle2_dx = random(low, high)
+  circle2_dy = random(low, high)
   
   // Reset colors
   circle1_color = "black"
