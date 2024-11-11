1702ee12eb8cf6e418664cc8777142a8b979dc6d
Matthew Lee <Matthew_Lee@avenues-ny.org>
2024-11-10 21:57:20 -0500

    Refractor Edit for Objects

--- sketch.js
+++ sketch.js
@@ -1,19 +1,18 @@
 // Variables creation
+  // Shapes
+  let circle1, circle2
 
-// Shapes
-// Circle 1 (Cannon)
-let circle1_x, circle1_y, circle1_dx, circle1_dy, circle1_color, circle1_s, circle1_r
-// Circle 2 (CannonBall)
-let circle2_x, circle2_y, circle2_dx, circle2_dy, circle2_color, circle2_s, circle2_r
-// Other Variables
-let w, distance, c, n, low, high
+  // Other Variables
+  let w, distance, c, n, low, high
 
 function setup() {  
   w = 400
   createCanvas(w, w)
+  
   // Max Color
   c = 255
-  // Flip to Negative
+  
+  // Flip to Negative Multiplier
   n = -1
 
   // Velocities
@@ -21,90 +20,69 @@ function setup() {
   high = 3
   
   // Circle 1 (Cannon) Variables
-  circle1_s = 60
-  circle1_r = 0.5 * circle1_s
-  circle1_color = "black"
-  
-  // Circle 2 (Cannonball) Variables
-  circle2_s = 20
-  circle2_r = 0.5 * circle2_s
-  circle2_color = "red"
-  
-  reset()
+  circle1 = createCircle(60,"black")
+  circle2 = createCircle(20,"red")
 }
 
 function draw() {
   background(220)
   
-  // Draw circles
-  fill(circle1_color)
-  circle(circle1_x, circle1_y, circle1_s)
-  
-  fill(circle2_color)
-  circle(circle2_x, circle2_y, circle2_s)
-  
-  // Move circles
-  circle1_x += circle1_dx
-  circle1_y += circle1_dy
-  circle2_x += circle2_dx
-  circle2_y += circle2_dy
-  
-  // Boundary Check: Circle 1
-  if (circle1_x - circle1_r < 0 || circle1_x + circle1_r > width) { 
-    circle1_dx *= n
-  }
-  
-  if (circle1_y - circle1_r < 0 || circle1_y + circle1_r > height) { 
-    circle1_dy *= n
-  }
-  
-  // Boundary Check: Circle 2
-  if (circle2_x - circle2_r < 0 || circle2_x + circle2_r > width) { 
-    circle2_dx *= n
-  }
-  
-  if (circle2_y - circle2_r < 0 || circle2_y + circle2_r > height) { 
-    circle2_dy *= n
-  }
+  updateCircle(circle1)
+  updateCircle(circle2)
   
   // Collision Check
-  distance = dist(circle1_x, circle1_y, circle2_x, circle2_y)
-  if (distance <= circle1_r + circle2_r) {
+  distance = dist(circle1.x, circle1.y, circle2.x, circle2.y)
+  if (distance <= circle1.r + circle2.r) {
     handleCollision()
   }
 }
 
-// Collision handling - changes color and makes circles go in opposite directions
+function createCircle(size, color) {
+  return{
+    x: random(w),
+    y: random(w),
+    dx: random(low,high),
+    dy: random(low,high),
+    s: size,
+    r: 0.5 * size,
+    color: color
+  }
+}
+
+function updateCircle(c) {
+  //Draw circle
+  fill(c.color)
+  circle(c.x, c.y, c.s)
+  
+  // Move Circle
+  c.x += c.dx
+  c.y += c.dy
+  
+  // Boundary Checks
+  if (c.x - c.r < 0 || c.x + c.r > width) {
+    c.dx *= n
+  }
+  if (c.y - c.r < 0 || c.y + c.r > height) {
+    c.dy *= n
+  }
+}
+
 function handleCollision() {
   // Change both circles' colors to random colors
-  circle1_color = color(random(c), random(c), random(c))
-  circle2_color = color(random(c), random(c), random(c))
+  circle1.color = color(random(c), random(c), random(c))
+  circle2.color = color(random(c), random(c), random(c))
   
-  // Make circles go in opposite directions by swapping and reversing velocities
-  circle1_dx *= n
-  circle1_dy *= n
-  
-  circle2_dx *= n
-  circle2_dy *= n
-
+  // Reverse the directions
+  circle1.dx *= n
+  circle1.dy *= n
+  circle2.dx *= n
+  circle2.dy *= n
 }
 
 // Reset function for key press
 function reset() {
-  // Set random initial positions and velocities
-  circle1_x = random(w)
-  circle1_y = random(w)
-  circle1_dx = random(low, high)
-  circle1_dy = random(low, high)
-  
-  circle2_x = random(w)
-  circle2_y = random(w)
-  circle2_dx = random(low, high)
-  circle2_dy = random(low, high)
-  
-  // Reset colors
-  circle1_color = "black"
-  circle2_color = "red"
+  circle1 = createCircle(60, "black")
+  circle2 = createCircle(20, "red")
 }
 
 // Key press to reset
